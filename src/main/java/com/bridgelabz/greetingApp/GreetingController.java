ebb94acba3c495cb38085b60eafb527f21ab8ba5
Tarun Dubey <tarun2019dubey@gmail.com>
2025-02-24 12:24:06 +0530

    [Tarun] Add. simple greeting

--- src/main/java/com/bridgelabz/greetingApp/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingApp/GreetingController.java
@@ -1,11 +1,22 @@
 package com.bridgelabz.greetingApp;
 
 import com.bridgelabz.greetingApp.greetingClass.Greeting;
+import com.bridgelabz.greetingApp.greetingClass.GreetingService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
+    private final GreetingService greetingService;
+
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/simpleGreeting")
+    public Greeting simpleGreeting() {
+        return new Greeting(greetingService.greet());
+    }
     @GetMapping("/hello")
     public Greeting greeting() {
         return new Greeting("Hello from Bridge Labz");
---
+++ src/main/java/com/bridgelabz/greetingApp/greetingClass/GreetingService.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.greetingApp.greetingClass;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService{
+    public String greet(){
+        return "Hello World";
+    }
+}
