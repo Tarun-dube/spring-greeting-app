2e607c53a492daa6a53d7ae384492a0ed06bb02c
Tarun Dubey <tarun2019dubey@gmail.com>
2025-02-24 16:59:12 +0530

    [Tarun] Add. get by id

--- src/main/java/com/bridgelabz/greetingApp/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingApp/GreetingController.java
@@ -1,12 +1,8 @@
 package com.bridgelabz.greetingApp;
 
 import com.bridgelabz.greetingApp.greetingClass.Greeting;
-
 import com.bridgelabz.greetingApp.repository.GreetingRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import com.bridgelabz.greetingApp.greetingClass.GreetingService;
-
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,43 +11,9 @@ import java.util.List;
 @RequestMapping("/greeting")
 public class GreetingController {
 
-    
+    @Autowired
     GreetingRepo repo;
 
-    private final GreetingService greetingService;
-
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
-
-    @GetMapping("/simpleGreeting")
-    public Greeting getGreeting(
-            @RequestParam(required = false) String firstName,
-            @RequestParam(required = false) String lastName) {
-
-        String message = greetingService.greet(firstName, lastName);
-        return new Greeting(message);
-    }
-
-    @GetMapping("/hello")
-    public Greeting greeting() {
-        return new Greeting("Hello from Bridge Labz");
-    }
-
-    @PostMapping("/hello")
-    public Greeting greetingPost(@RequestBody Greeting request){
-        return new Greeting("Received: " + request.getGreeting()) ;  }
-
-    @PutMapping("/hello/{name}")
-    public Greeting greetingPut(@PathVariable String name, @RequestParam String lastName){
-       return new Greeting("Updated: " + name + " " + lastName) ;
-    }
-
-    @DeleteMapping("/hello/{id}")
-    public Greeting greetingDelete(@PathVariable String id){
-
-        return new Greeting("Deleted: " + id);
-    }
 
     @GetMapping("/hi")
     public List<Greeting> getAllGreetings(){
@@ -60,8 +22,12 @@ public class GreetingController {
 
     @PostMapping("/hi")
     public void addGreeting(@RequestBody Greeting greeting){
-
         repo.save(greeting);
     }
+    @GetMapping("/hi/{id}")
+        public Greeting getById(@PathVariable int id){
+            return repo.findById(id).get();
+        }
+
 
 }
