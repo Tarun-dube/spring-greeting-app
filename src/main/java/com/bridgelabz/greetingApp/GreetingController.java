c0bffaaeeb2a859f0813671da7ca064dcae85bb3
Tarun Dubey <tarun2019dubey@gmail.com>
2025-02-24 16:22:24 +0530

    [Tarun] Add. add to repository

--- src/main/java/com/bridgelabz/greetingApp/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingApp/GreetingController.java
@@ -1,11 +1,20 @@
 package com.bridgelabz.greetingApp;
 
 import com.bridgelabz.greetingApp.greetingClass.Greeting;
+import com.bridgelabz.greetingApp.repository.GreetingRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
+
+    @Autowired
+    GreetingRepo repo;
+
+
     @GetMapping("/hello")
     public Greeting greeting() {
         return new Greeting("Hello from Bridge Labz");
@@ -22,6 +31,18 @@ public class GreetingController {
 
     @DeleteMapping("/hello/{id}")
     public Greeting greetingDelete(@PathVariable String id){
+
         return new Greeting("Deleted: " + id);
     }
+
+    @GetMapping("/hi")
+    public List<Greeting> getAllGreetings(){
+        return repo.findAll();
+    }
+
+    @PostMapping("/hi")
+    public void addGreeting(@RequestBody Greeting greeting){
+         repo.save(greeting);
+    }
+
 }
