52456d529d4cd51cf5b9004d9c8fe9078c8d5566
Tarun Dubey <tarun2019dubey@gmail.com>
2025-02-24 12:39:10 +0530

    [Tarun] Add. greeting name

--- src/main/java/com/bridgelabz/greetingApp/greetingClass/GreetingService.java
+++ src/main/java/com/bridgelabz/greetingApp/greetingClass/GreetingService.java
@@ -4,7 +4,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService{
-    public String greet(){
+    public String greet(String firstName, String lastName){
+        if(firstName!=null && lastName!=null){
+            return "Hello "+firstName+" "+lastName;
+        }
+        else if(firstName!=null && lastName==null){
+            return "Hello "+firstName;
+        }
+        else if(firstName==null&&lastName!=null){
+            return "Hello "+lastName;
+        }
+
         return "Hello World";
     }
 }
