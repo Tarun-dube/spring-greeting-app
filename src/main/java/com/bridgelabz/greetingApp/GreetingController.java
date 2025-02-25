9c10b0ba6edf6a3ab51308a83977d9a978fa7dbb
Tarun Dubey <tarun2019dubey@gmail.com>
2025-02-25 10:41:33 +0530

    [Tarun] Fix. fix the code

--- src/main/java/com/bridgelabz/greetingApp/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingApp/GreetingController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.greetingApp;
 
 import com.bridgelabz.greetingApp.greetingClass.Greeting;
+import com.bridgelabz.greetingApp.greetingClass.GreetingService;
 import com.bridgelabz.greetingApp.repository.GreetingRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,40 @@ public class GreetingController {
     @Autowired
     GreetingRepo repo;
 
+    private final GreetingService greetingService;
+
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/simpleGreeting")
+    public Greeting getGreeting(
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName) {
+
+        String message = greetingService.greet(firstName, lastName);
+        return new Greeting(message);
+    }
+
+    @GetMapping("/hello")
+    public Greeting greeting() {
+        return new Greeting("Hello from Bridge Labz");
+    }
+
+    @PostMapping("/hello")
+    public Greeting greetingPost(@RequestBody Greeting request){
+        return new Greeting("Received: " + request.getGreeting()) ;  }
+
+    @PutMapping("/hello/{name}")
+    public Greeting greetingPut(@PathVariable String name, @RequestParam String lastName){
+        return new Greeting("Updated: " + name + " " + lastName) ;
+    }
+
+    @DeleteMapping("/hello/{id}")
+    public Greeting greetingDelete(@PathVariable String id){
+
+        return new Greeting("Deleted: " + id);
+    }
 
     @GetMapping("/hi")
     public List<Greeting> getAllGreetings(){
